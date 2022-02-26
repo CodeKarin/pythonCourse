0b623af5ca99271d9581fa1e21e1745ff1d4f34e
CodeKarin <58594501+CodeKarin@users.noreply.github.com>
2022-02-26 10:09:46 +0100

    Add files via upload

---
+++ Day2Solutions/Ex_1_a/animals/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 21 15:53:27 2022
+
+@author: karwe916
+"""
+
+from .mammals import Mammals
+from .birds import Birds
---
+++ Day2Solutions/Ex_1_a/test_animals.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 21 15:51:05 2022
+
+@author: karwe916
+"""
+
+import animals
+
+
+myMammal = animals.Mammals()
+myMammal.printMembers()
+
+myBird = animals.Birds()
+myBird.printMembers()
