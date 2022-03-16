49cefe091b78ad22f1dd2ecf6f893d6df287dc86
CodeKarin <58594501+CodeKarin@users.noreply.github.com>
2022-03-16 08:33:14 +0100

    Delete ExercisesSolutions directory

--- ExercisesSolutions/helloworld.pyx
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Mar  4 11:25:00 2022
-
-@author: karwe916
-"""
-
-print("Hello World")
--- ExercisesSolutions/helloworld_main.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Mar  4 13:16:25 2022
-
-@author: karwe916
-"""
-
-import helloworld                                                                                                                    
